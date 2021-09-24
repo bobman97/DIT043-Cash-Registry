b00e07fd1f6511ce28183f82a27012ebfeccecdd
bobman97 <gushilwi@student.gu.se>
2021-09-24 16:28:59 +0200

    itemOptions
    made some struff private.

--- CashRegisterFail/src/main/java/ItemOptions/Item.java
+++ CashRegisterFail/src/main/java/ItemOptions/Item.java
@@ -2,9 +2,9 @@ package ItemOptions;
 
 public class Item {
     // This will be the objects that stores the information about all the items in the shop.
-    public int id;
-    public String name;
-    public double price;
+    final int id;
+    String name;
+    double price;
 
     public Item(int id, String name, double price)   {
         this.id = id;
